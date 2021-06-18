8f25c869032ba482447f238c597db3051160f120
DanielWeide <danielkwang@outlook.com>
2021-06-18 14:32:28 +0800

    Compute and Get Count For Each Number

--- src/NumberStatCounterProgramme.java
+++ src/NumberStatCounterProgramme.java
@@ -12,9 +12,20 @@ public class NumberStatCounterProgramme {
 
 		    //now read the file line by line...
 		    int lineNum = 0;
+		    int actualNum=0;
+		    char[] numAtTheString = new char[4];
+		    int[] numStat = new int[10];
+		    
 		    while (scanner.hasNextLine()) {
-		        String line = scanner.nextLine();
-		        System.out.println("ho hum, i found it on line " +line);
+		        String line = scanner.nextLine(); // get the number from the line
+		        //actualNum = Integer.parseInt(line);
+		        // extracting the number
+		        for(int i=0;i<4;i++) {
+		        	numAtTheString[i] = (char) Character.getNumericValue(line.charAt(i));
+		        	int j = numAtTheString[i];
+		        	numStat[j] +=1;
+		        }
+		        System.out.println(line);
 		        lineNum++; 
 		    }
 		} catch(FileNotFoundException e) { 
